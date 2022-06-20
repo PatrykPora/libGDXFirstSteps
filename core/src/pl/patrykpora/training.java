f0d4e53904ceb915f27ae90bb62d5e9dc29693d3
unknown <patryk_pora@o2.pl>
2022-06-20 22:04:32 +0200

    project context

---
+++ core/src/pl/patrykpora/training.java
@@ -0,0 +1,32 @@
+package pl.patrykpora;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+
+
+public class training extends ApplicationAdapter {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+
+    @Override
+    public void dispose() {
+
+    }
+}
